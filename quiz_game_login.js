1364788b18ae0ba01963aec5a4f0a6995798d792
gwynethmachado <130666463+gwynethmachado@users.noreply.github.com>
2023-11-13 17:34:19 +0400

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,27 @@
+addUser()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function send() {
+    number1 = document.getElementById("number1").value ;
+    number1 = document.getElementById("number2").value ;
+    actual_answer = parseInt(number1) * parseInt(number2);    
+    question_number = "<h4>" + "X"+ number2 +"</h4>";
+    input_box = "<br>Answer : <input type'text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+}
+
